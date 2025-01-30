b848369bc24eb62c4f738de017137365f4c5dac3
EnzoPeralta96 <enzash.k@gmial.com>
2025-01-30 12:25:22 -0300

    Agregando datos utilizando el dbContext

--- Wpm.Client/Program.cs
+++ Wpm.Client/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using SQLitePCL;
 using Wpm.Dal;
+using Wpm.Domain;
 
 
 var builder = new ConfigurationBuilder();
@@ -10,5 +11,56 @@ var config = builder.Build();
 Batteries.Init();
 
 using var db = new WpmDbContext(config);
+db.Database.EnsureDeleted();
 db.Database.EnsureCreated();
+
+//Creo especies
+var perro = new Species(){ Name = "Perro"};
+var gato = new Species(){ Name = "Gato"};
+
+//Agrego las especies al dbSet
+db.Species.Add(perro);
+db.Species.Add(gato);
+
+db.SaveChanges();
+
+//Creo razas
+//Paso el objeto perro, para indicar la fk
+var beagle = new Breed(){ Name = "Beagle", Species = perro};
+
+//Pero tambien le puedo pasar el valor de la fk directamente
+var pitbull = new Breed(){ Name = "Pitbull", SpeciesId = 1};
+var siames = new Breed(){Name = "Siamés", SpeciesId = 2};
+
+db.Breeds.Add(beagle);
+db.Breeds.Add(pitbull);
+db.Breeds.Add(siames);
+
+db.SaveChanges();
+
+//Agregando mascotas:
+var gianni = new Pet(){ Name = "Gianni" , Breed = beagle};
+var cati = new Pet(){ Name = "Cati", BreedId = 2};
+
+db.Pets.Add(gianni);
+db.Pets.Add(cati);
+
+db.SaveChanges();
+
+//Agregando dueños de las mascotas- relacion n:n
+var rodrigo = new Owner(){Name = "Rodrigo"};
+var leonardo = new Owner(){ Name = "Leonardo"};
+
+//En este caso EF detecta la relación entre Pet y Owner
+//Agrega los dueños a la tabla Owner y luego los relaciona
+//Notar que no agregamos los dueños a su dbSet y aún asi funciono.
+gianni.Owners.Add(rodrigo);
+gianni.Owners.Add(leonardo);
+cati.Owners.Add(rodrigo);
+cati.Owners.Add(leonardo);
+
+db.SaveChanges();
+
+
+Console.WriteLine("Listo");
 
